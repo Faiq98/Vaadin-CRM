a3310e34c1cff91f30c5e1cbddc56492839781b4
Faiq98 <fh.faiq@gmail.com>
2021-02-07 21:13:17 +0800

    update some code

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,12 +1,30 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 //@Route will map the view to the root
 @Route("")
 public class MainView extends VerticalLayout{
-    public MainView(){
 
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+
+    public MainView(){
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
     }
+
+    private void configureGrid(){
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("first_name", "last_name", "email", "status");
+    }
+
 }
